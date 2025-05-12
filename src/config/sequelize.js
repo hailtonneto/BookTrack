ba6349707028b17bc098e7d85f728dfd7b9c482d
hailtonneto <hailtonneto@gmail.com>
2025-05-12 19:04:04 -0300

    gitignore atualizado corretamente

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "postgres", // ou 'mysql', 'sqlite', dependendo do banco usado
+    dialect: "sql", // ou 'mysql', 'sqlite', dependendo do banco usado
   }
 );
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express();
 const sequelize = require("./config/sequelize");
 const usuarioRoutes = require("./routes/usuarios");
 
+require('dotenv').config();
+
 app.use(express.json());
 app.use("/usuarios", usuarioRoutes);
 
@@ -12,7 +14,7 @@ app.listen(PORT, async () => {
   try {
     await sequelize.authenticate();
     console.log("Conexão com o banco de dados bem-sucedida!");
-    await sequelize.sync({ force: false }); // Sincroniza o banco sem apagar dados
+    await sequelize.sync({ force: false });
     console.log(`Servidor rodando na porta ${PORT}`);
   } catch (error) {
     console.error("Erro ao conectar com o banco de dados:", error);
