b31c3c829efd40f88544d2dfaf0dabba3067eddc
hailtonneto <hailtonneto@gmail.com>
2025-05-12 22:29:08 -0300

    Tentando finalizar o BD

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,15 +3,15 @@ const path = require("path");
 
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
-console.log(process.env, 'PROCESS ENV');
-
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME,     // Nome do banco
+  process.env.DB_USER,     // Usuário do banco
+  process.env.DB_PASSWORD, // Senha do banco
   {
-    host: process.env.DB_HOST,
-    dialect: "postgres", // ou 'mysql', 'sqlite', dependendo do banco usado
+    host: process.env.DB_HOST,   // Host do banco (ex.: localhost)
+    port: process.env.DB_PORT,   // Porta do banco (ex.: 5432)
+    dialect: "postgres",         // Dialeto para PostgreSQL
+    logging: console.log,        // Mostra as queries no console
   }
 );
 
